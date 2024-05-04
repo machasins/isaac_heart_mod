0a668e7e3977c40c51987c9606e12b9d8a5a98cf
Michael Kinyon <michaelkinyon@gmail.com>
2024-05-03 22:45:51 -0700

    fully fixed blended hearts

--- main.lua
+++ main.lua
@@ -53,20 +53,26 @@ end
 
 mod:AddCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, mod.OnPickupCollision)
 
----comment
+---Spawn an amount of hearts
 ---@param amount integer
 ---@param half HeartSubType
 ---@param full HeartSubType
 ---@param player EntityPlayer
 local function SpawnHearts(amount, half, full, player)
+    -- While the amount of hearts to spawn is greater than 0
     while amount > 0 do
+        -- If the amount can support a full heart
         if amount >= 2 then
+            -- Spawn a full heart at a random postion
             Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_HEART, full,
                 Game():GetRoom():FindFreePickupSpawnPosition(player.Position, 0, true), Vector.Zero, player)
+            -- Reduce the amount to spawn by 2
             amount = amount - 2
         else
+            -- Spawn a half heart at a random position
             Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_HEART, half,
                 Game():GetRoom():FindFreePickupSpawnPosition(player.Position, 0, true), Vector.Zero, player)
+            -- Reduce the amount to spawn by 1
             amount = amount - 1
         end
     end
@@ -85,28 +91,39 @@ function mod:OnUpdate()
             -- Check if the heart should be processed and that the player is not a Lost variant (Can't get health)
             if not mod.IGNORE_LIST[type] and player and player:GetHealthType() ~= HealthType.LOST then
                 local playerType = player:GetPlayerType()
-                -- Check if the heart has a soul value 
+                -- Check if the heart is a blended heart
                 if mod.BOTH_VALUES[type] and playerType ~= PlayerType.PLAYER_BETHANY and playerType ~= PlayerType.PLAYER_BETHANY_B then
+                    -- The excess amount of red hearts
                     local excessRed = (playerData.heartAmnt + mod.BOTH_VALUES[type]) - player:GetHearts()
+                    -- The excess amount of soul hearts
                     local excessSoul = (playerData.soulAmnt + mod.BOTH_VALUES[type]) - player:GetSoulHearts()
+                    -- Whether to spawn any hearts
                     local doSpawn = (excessRed + excessSoul) ~= mod.BOTH_VALUES[type]
-                    print(excessRed, excessSoul, doSpawn)
-                    if doSpawn and excessRed > 0 then
+                    -- Spawn red hearts
+                    if doSpawn and excessRed < excessSoul then
                         SpawnHearts(excessRed, HeartSubType.HEART_HALF, HeartSubType.HEART_FULL, player)
-                    elseif doSpawn and excessSoul > 0 then
+                    -- Spawn soul hearts
+                    elseif doSpawn and excessSoul < excessRed then
                         SpawnHearts(excessSoul, HeartSubType.HEART_HALF_SOUL, HeartSubType.HEART_SOUL, player)
                     end
+                -- Check if the heart is a type of soul heart and the player is not Bethany
                 elseif mod.SOUL_VALUES[type] and playerType ~= PlayerType.PLAYER_BETHANY then
+                    -- The amount of hearts to spawn
                     local excessAmnt = (playerData.soulAmnt + mod.SOUL_VALUES[type]) - player:GetSoulHearts()
+                    -- Spawn the extra hearts
                     SpawnHearts(excessAmnt, HeartSubType.HEART_HALF_SOUL, HeartSubType.HEART_SOUL, player)
+                -- Check if the heart is a type of red heart and the player is not T. Bethany
                 elseif mod.HEART_VALUES[type] and playerType ~= PlayerType.PLAYER_BETHANY_B then
+                    -- The amount of hearts to spawn
                     local excessAmnt = (playerData.heartAmnt + mod.HEART_VALUES[type]) - player:GetHearts()
+                    -- Spawn the extra hearts
                     SpawnHearts(excessAmnt, HeartSubType.HEART_HALF, HeartSubType.HEART_FULL, player)
                 end
             end
         end
     end
 
+    -- Reset the pickup list
     pickupList = {}
 end
 
